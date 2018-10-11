4bc2bbea6dbd6fe5b9edd559f177b2084414852b
Thomas Coats <github@voodoolabs.net>
2018-10-11 21:52:54 +1300

    event store

--- es.js
+++ es.js
@@ -97,13 +97,34 @@ module.exports = (db, opts) => {
           else
             puts[k] = Object.assign(result, puts[k])
         }
-        ops = []
+        const dbops = []
         for (let k of Object.keys(deletes))
-          ops.push({ type: 'del', key: k })
+          dbops.push({ type: 'del', key: k })
         for (let k of Object.keys(puts))
-          ops.push({ type: 'put', key: k, value: JSON.stringify(puts[k]) })
-        resolve(db.batch(ops))
+          dbops.push({ type: 'put', key: k, value: JSON.stringify(puts[k]) })
+        db.batch(dbops).then(() => resolve(ops))
       })
     })
   }
+}
+module.exports.diff = (store, source, target) => {
+  const ops = []
+  for (let id of Object.keys(source)) {
+    const s = source[id]
+    if (!target[id]) {
+      ops.push({ type: 'del', store: store, id: id })
+      continue
+    }
+    const t = target[id]
+    const d = {}
+    for (let key of Object.keys(s))
+      if (!t[key]) d[key] = null
+    for (let key of Object.keys(t))
+      if (!s[key]) d[key] = t[key]
+      else if (s[key] !== t[key]) d[key] = t[key]
+    ops.push({ type: 'put', store: store, id: id, value: d })
+  }
+  for (let id of Object.keys(target)) if (!source[id])
+    ops.push({ type: 'add', store: store, id: id, value: target[id] })
+  return ops
 }
